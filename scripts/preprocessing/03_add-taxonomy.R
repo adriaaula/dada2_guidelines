74de51ba4cafc6d5b1cbc86db8d6c08595e10a83
aleixop <aleix.obiol@gmail.com>
2024-09-04 11:19:41 +0200

    solved handling of dbs with different num of ranks

--- scripts/preprocessing/03_add-taxonomy.R
+++ scripts/preprocessing/03_add-taxonomy.R
@@ -2,6 +2,7 @@
 #This script adds taxonomy to each of the inferred ASVs
 
 library(dada2)
+library(Biostrings)
 library(tidyverse)
 
 cat(paste0('\n',"You are using DADA2 version ", packageVersion('dada2'),'\n'))
@@ -29,39 +30,58 @@ seqtab.nochim <- readRDS(seqtab.nochim)
 
 if (grepl('[Dd]ecipher|DECIPHER', method)){ # use decipher
 
-library(DECIPHER)
-cat(paste0("You are using DECIPHER version ", packageVersion('DECIPHER'),'\n\n'))
+  library(DECIPHER)
+  cat('# The taxonomic classification included in decipher will be used\n')
+  cat(paste0("# You are using DECIPHER version ", packageVersion('DECIPHER'),'\n'))
+  cat('################################\n')
 
-dna <- DNAStringSet(getSequences(seqtab.nochim)) # Create a DNAStringSet from the ASVs
+  dna <- DNAStringSet(getSequences(seqtab.nochim)) # Create a DNAStringSet from the ASVs
 
-if (grepl('\\.RData$', tax_db[1])){
-  load(tax_db[1])
-} else if (grepl('\\.rds$',tax_db[1])){
-  trainingSet <- readRDS(tax_db[1])
-}
+  if (grepl('\\.RData$', tax_db[1])){
+    load(tax_db[1])
+  } else if (grepl('\\.rds$',tax_db[1])){
+    trainingSet <- readRDS(tax_db[1])
+  }
 
-if (is.na(threshold)){
-    threshold <- 60
-}
+  if (is.na(threshold)){
+      threshold <- 60
+  }
 
-ids <- IdTaxa(dna,
-             trainingSet,
-             strand = "top",
-             processors = NULL,
-             verbose = FALSE,
-             threshold = threshold)
+  ids <- IdTaxa(dna,
+              trainingSet,
+              strand = "top",
+              processors = NULL,
+              verbose = FALSE,
+              threshold = threshold)
 
-ranks <- c("domain", "phylum", "class", "order", "family", "genus", "species") # ranks of interest
+  ranks <- paste0('level',1:max(trainingSet$levels))
 
-# Convert the output object of class "Taxa" to a matrix analogous to the output from assignTaxonomy
-taxid <- t(sapply(ids, function(x) {
-        m <- match(ranks, x$rank)
-        taxa <- x$taxon[m]
-        taxa[startsWith(taxa, "unclassified_")] <- NA
-        taxa
-}))
+  # Convert the output object of class "Taxa" to a matrix analogous to the output from assignTaxonomy
 
-colnames(taxid) <- ranks; rownames(taxid) <- getSequences(seqtab.nochim)
+  taxid <-
+    names(ids) %>%
+    map_df(~ tibble(seq_name = .x, tax = ids[[.x]]$taxon, ranks = ranks[1:length(ids[[.x]]$taxon)])) %>%
+    pivot_wider(names_from = ranks, values_from=tax)
+
+  write_tsv(taxid, paste0(output, name, "_tax_assignation_decipher.txt"))
+
+  # Convert learnTaxa to data frame too
+
+  n_seq <- length(ids)
+  df_rows <- list()
+
+  for(i in 1:n_seq){
+    seq_name <- names(ids[i])
+    taxonomy<- ids[[i]]$taxon
+    confidence <- ids[[i]]$confidence
+    df_rows[[i]] = data.frame(seq_name, taxonomy, confidence, taxo_level=ranks[1:length(ids[[i]]$taxon)])
+  }
+
+  df <- purrr::reduce(df_rows, bind_rows) %>%
+    filter(taxo_level != "Root") %>%
+    pivot_wider(names_from = taxo_level, values_from = c(taxonomy, confidence))
+
+    write_tsv(df, paste0(output, name, "_decipher_output.txt"))
 
 } else { # use regular dada2 classificator
   
@@ -70,24 +90,45 @@ colnames(taxid) <- ranks; rownames(taxid) <- getSequences(seqtab.nochim)
   }
 
   cat('# The taxonomic classification included in dada2 will be used\n')
+  cat(paste0('\n',"# You are using DADA2 version ", packageVersion('dada2'),'\n'))
+  cat('################################\n')
+
+  n_ranks <- length(str_split(str_remove(names(readDNAStringSet(tax_db[1], nrec = 1)),';$'),';')[[1]])
+  ranks <- paste0('level',1:n_ranks)
+
   taxid <- assignTaxonomy(seqtab.nochim,
                         tax_db[1], 
                         multithread=TRUE,
-                        minBoot=threshold)
+                        minBoot=threshold,
+                        taxLevels = ranks,
+                        outputBootstraps = TRUE)
   cat('# Taxonomy assigned to genus level\n')
   
   if (!is.na(tax_db[2])) { # add species level if db available
-    taxid <- addSpecies(taxid, 
+    taxid <- addSpecies(taxid$tax, 
                         tax_db[2], 
                         verbose=TRUE, 
                         allowMultiple=3)
-    cat('\n# Taxonomy assigned to species level\n')
+    cat('\n# Taxonomy assigned to species level\n')    
   }
+
+  rownames(taxid$tax) <- colnames(seqtab.nochim)
+  rownames(taxid$boot) <- colnames(seqtab.nochim)
+
+  tax_table <-
+    taxid$tax %>%
+    as_tibble(rownames = 'seq_name')
+  
+  boot_table <-
+    taxid$boot %>%
+    as_tibble(rownames = 'seq_name')
+  
+  write_tsv(tax_table, paste0(output, name, "_tax_assignation_dada.txt"))
+  write_tsv(boot_table, paste0(output, name, "_dada_output.txt"))
+
 }
 
-# Write to disk
-saveRDS(taxid, paste0(output, name, "_tax_assignation.rds"))
-
+# Farewell
 cat('\n')
-cat(paste0('# The obtained taxonomy file can be found in "', paste0(output, name, "_tax_assignation.rds"), '"\n'))
+cat(paste0('# The obtained taxonomy file can be found in "', paste0(output, name, "_tax_assignation_",method,".txt"), '"\n'))
 cat('\n# All done!\n\n')
